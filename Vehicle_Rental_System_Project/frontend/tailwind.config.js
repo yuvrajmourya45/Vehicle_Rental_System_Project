e002b174eaf032d05f501c81356ebeaffa95953b
Yuvraj Singh Mourya <yuvrajsinghmourya185@gmail.com>
2026-03-02 21:03:46 +0530

    responsive

--- Vehicle_Rental_System_Project/frontend/tailwind.config.js
+++ Vehicle_Rental_System_Project/frontend/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+      },
+      fontSize: {
+        'xxs': '0.625rem',
+      },
+    },
   },
   plugins: [],
 }
